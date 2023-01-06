b8a0f0c27f2f3c0317d40c4932b64c598a82175c
Kadir Engin <kadirengin624@hotmail.com>
2023-01-07 02:12:23 +0300

    add user model and update other models.

--- models/model.go
+++ models/model.go
@@ -1,12 +1,31 @@
 package models
 
+import "time"
+
 type Poll struct {
-	ID           int       `json:"id"`
+	ID           string    `json:"id"`
+	UserId       string    `json:"userId"`
 	PollQuestion string    `json:"pollQuestion"`
 	PollOptions  []Options `json:"options"`
+	CreatedAt    time.Time `json:"createdAt" bson:"createdAt"`
 }
 
 type Options struct {
-	OptionText string `json:"optionText"`
-	Responses  int    `json:"responses"`
+	OptionText  string `json:"optionText"`
+	OptionVotes int    `json:"optionVotes"`
+}
+
+type User struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	Password     []byte    `json:"password"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UserVotes    []Votes   `json:"userVotes"`
+	ProfilePhoto string    `json:"profilePhoto"`
+}
+
+type Votes struct {
+	PollId string `json:"pollId"`
+	Vote   string `json:"vote"`
 }
